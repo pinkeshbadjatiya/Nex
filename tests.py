622d646e7be202e6f4e8ac754554c762f960f8ef
Pinkesh Badjatiya <pinkeshbadjatiya@gmail.com>
2016-04-09 15:00:58 +0530

    Migrate to nose2 + Add 2 tests
    - Migrate to nose2 to support py2 and py3 versions
    - Add 2 new tests
    - Update .travis.yml (Related to nose2)
    - Add nose2 in requirements.txt
    - Delete redundant code (temp.py)

--- tests.py
+++ tests.py
@@ -2,14 +2,41 @@ import utils
 import operator
 import json
 import os
-import nose.tools
+import nose2
+import re
 
-TEST_DIRECTORY="./sample_test_files/"
+TEST_DIRECTORY = "./sample_test_files/"
+CURRENT_DIRECTORY = "./"
+IP_REGEX = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
 
 def test_loadConfig():
+    expected = '{"STATUS_STRING": {"200": "200 OK", "404": "404 Not Found", "403": "403 Forbidden", "500": "500 Server Error"}, "MAX_CLIENT_QUEUE": 5, "SERVER_NAME": "Nex - Simple HTTP server/v.0.2", "HOST_ALLOWED": ["*"], "BIND_PORT": 12345, "ERROR_DIR": "./error_pages", "MAX_REQUEST_LEN": 1024, "SERVER_SHORT_NAME": "Nex/v.0.2", "HOST_NAME": "localhost", "PUBLIC_HTML": "./public_html", "OTHER_TEMPLATES": "./other_templates"}'
     config = utils.loadConfig(TEST_DIRECTORY + 'settings.conf')
-    nose.tools.eq_(json.dumps(config), '{"STATUS_STRING": {"200": "200 OK", "404": "404 Not Found", "403": "403 Forbidden", "500": "500 Server Error"}, "MAX_CLIENT_QUEUE": 5, "SERVER_NAME": "Nex - Simple HTTP server/v.0.2", "HOST_ALLOWED": ["*"], "BIND_PORT": 12345, "ERROR_DIR": "./error_pages", "MAX_REQUEST_LEN": 1024, "SERVER_SHORT_NAME": "Nex/v.0.2", "HOST_NAME": "localhost", "PUBLIC_HTML": "./public_html", "OTHER_TEMPLATES": "./other_templates"}')
+    assert json.dumps(config) == expected, "loadConfig: Failed to load the configFile."
+
+def test_directory_check():
+    config = utils.loadConfig(CURRENT_DIRECTORY + 'settings.conf')
+
+    # Check for PUBLIC_HTML, ERROR_DIR, OTHER_TEMPLATES
+    for key in ['PUBLIC_HTML', 'ERROR_DIR', 'OTHER_TEMPLATES']:
+        config[key] = config[key].strip(" ")
+        assert config[key]!="", key + ": Field cannot be left blank"
+        assert config[key][-1]!="/", key + ": Directory name must not end with a '/'"
+
+
+def test_valid_hostname():
+    config = utils.loadConfig(CURRENT_DIRECTORY + 'settings.conf')
+    hostname = config['HOST_NAME']
+    if len(hostname) > 15:
+        raise AssertionError('HOST_NAME: hostname cannot be more than 15 character long.')
+
+    try:
+        assert hostname == "localhost", "HOST_NAME: Invalid Host Name"
+        return
+    except:
+        allowed = re.compile(IP_REGEX, re.IGNORECASE)
+        assert allowed.match(hostname), "HOST_NAME: Invalid Host Name"
 
 
 if __name__ == "__main__":
-    os.system("nosetests --verbosity=3 -x .")
+    os.system("nose2")
